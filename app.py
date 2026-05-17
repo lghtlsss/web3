bfd72d06b1814055c2d6276be8e4e6596beda9f8
1lght <guryanovartem2008@gmail.com>
2026-05-17 18:02:17 +0300

    task 13

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from data.db_session import create_session, global_init
 from jobs_request import get_jobs
+from data.register_form import RegisterForm
+from data.users import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super_secret_key'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    session = create_session()
     context = {'title': '', 'works_list': get_jobs(session)}
     return render_template('index.html', **context)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    session = create_session()
+    if form.validate_on_submit():
+        if form.password.data == form.repeat_password.data:
+            if 0 <= int(form.age.data) <= 120:
+                user = User(
+                    email=form.login.data,
+                    surname=form.surname.data,
+                    name=form.name.data,
+                    age=form.age.data,
+                    position=form.position.data,
+                    speciality=form.speciality.data,
+                    address=form.address.data
+                )
+                user.set_password(form.password.data)
+                session.add(user)
+                session.commit()
+                return redirect('/index')
+            return render_template('register.html', form=form, message='Такой возраст невозможен')
+        return render_template('register.html', form=form, message='Пароли не совпадают')
+    return render_template('register.html', form=form)
+
+
 if __name__ == '__main__':
     global_init('db/mars.sqlite')
-    session = create_session()
     app.run(port=8080, host='127.0.0.1', debug=False)
--- data/register_form.py
+++ data/register_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField
 from wtforms.validators import DataRequired
 
 
@@ -7,10 +7,10 @@ class RegisterForm(FlaskForm):
     login = StringField('login/email', validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     repeat_password = PasswordField("Repeat password", validators=[DataRequired()])
-    surname = PasswordField("Surname", validators=[DataRequired()])
-    name = PasswordField("Name", validators=[DataRequired()])
-    age = PasswordField("Age", validators=[DataRequired()])
-    position = PasswordField("Position", validators=[DataRequired()])
-    speciality = PasswordField("Speciality", validators=[DataRequired()])
-    address = PasswordField("Address", validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    age = IntegerField("Age", validators=[DataRequired()])
+    position = StringField("Position", validators=[DataRequired()])
+    speciality = StringField("Speciality", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
     submit = SubmitField('Submit')
